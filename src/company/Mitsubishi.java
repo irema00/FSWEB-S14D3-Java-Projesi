5d528e5596c16d1e82744db460d0bf0b2eed1c5e
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-23 23:16:28 +0300

    adds Mitsubishi sub-class

---
+++ src/company/Mitsubishi.java
@@ -0,0 +1,25 @@
+package company;
+
+public class Mitsubishi extends Car {
+    public Mitsubishi(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public String startEngine() {
+        super.startEngine();
+        return getName() + " engine is starting!";
+    }
+
+    @Override
+    public String accelerate() {
+        super.accelerate();
+        return getName() + " is accelerating!";
+    }
+
+    @Override
+    public String brake() {
+        super.brake();
+        return getName() + " is braking!";
+    }
+}
