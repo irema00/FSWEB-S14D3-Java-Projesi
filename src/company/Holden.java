26235d7eaca36e3bc982d60b58660363f0ccba99
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-23 23:18:02 +0300

    adds Holden sub-class

---
+++ src/company/Holden.java
@@ -0,0 +1,25 @@
+package company;
+
+public class Holden extends Car {
+    public Holden(String name, int cylinders) {
+        super(name, cylinders);
+    }
+    @Override
+    public String startEngine() {
+        super.startEngine();
+        return getName() + " engine is starting!";
+    }
+
+    @Override
+    public String accelerate() {
+        super.accelerate();
+        return getName() + " is accelerating!";
+    }
+
+    @Override
+    public String brake() {
+
+        super.brake();
+        return getName() + " is braking!";
+    }
+}
