96d8f37a58edac1b302fe5479e666e4b6cecdee8
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-23 23:17:19 +0300

    adds Ford sub-class

---
+++ src/company/Ford.java
@@ -0,0 +1,26 @@
+package company;
+
+public class Ford extends Car {
+    public Ford(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public String startEngine() {
+        super.startEngine();
+        return getName() + " engine is starting!";
+    }
+
+    @Override
+    public String accelerate() {
+        super.accelerate();
+        return getName() + " is accelerating!";
+    }
+
+    @Override
+    public String brake() {
+
+        super.brake();
+        return getName() + " is braking!";
+    }
+}
