efd76914c51655506e72b8c5fa6e29df320cf3f7
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-23 23:22:15 +0300

    tests Car and its subclasses

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import company.Car;
+import company.Ford;
+import company.Holden;
+import company.Mitsubishi;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car("Base car", 8);
+
+        printStartEngine(car);
+
+        System.out.println(car.accelerate());
+
+        System.out.println(car.brake());
+
+        Car mitsubishi = new Mitsubishi("Outlander VRX 4WD", 6);
+
+        printStartEngine(mitsubishi);
+
+        System.out.println(mitsubishi.accelerate());
+
+        System.out.println(mitsubishi.brake());
+
+        Car ford = new Ford("Ford Falcon", 6);
+
+        printStartEngine(mitsubishi);
+
+        System.out.println(ford.accelerate());
+
+        System.out.println(ford.brake());
+
+        Car holden = new Holden("Holden Commodore", 6);
+
+        printStartEngine(mitsubishi);
+
+        System.out.println(holden.accelerate());
+
+        System.out.println(holden.brake());
+    }
+
+
+    private static void printStartEngine(Car car) {
     }
 }
